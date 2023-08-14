a68ce30a28cf0d404f166c6354393d767434fe07
trismagp <tris.magp@gmail.com>
2023-08-14 23:53:01 +0200

    display posts in user index.html

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Profile
 from .forms import UserEditForm, ProfileEditForm
+from posts.models import Post
 
 def user_login(request):
     if request.method == "POST":
@@ -27,7 +28,9 @@ def user_login(request):
 
 @login_required
 def index(request):
-    return render(request, 'users/index.html')
+    current_user = request.user
+    posts = Post.objects.filter(user=current_user)
+    return render(request, 'users/index.html',{'posts':posts})
 
 def register(request):
     if request.method == 'POST':
