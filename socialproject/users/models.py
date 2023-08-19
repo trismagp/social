c062ce50a99488349e052b79fd30eed24f508f83
trismagp <tris.magp@gmail.com>
2023-08-19 23:10:14 +0200

    style user index page

--- socialproject/users/models.py
+++ socialproject/users/models.py
@@ -5,7 +5,7 @@ class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='users/%Y%m%d', blank= True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank= True)
 
     def __str__(self):
         return self.user.username
